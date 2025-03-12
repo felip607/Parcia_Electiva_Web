6eb9e1b3b034a4dc776e6ddecbbda5705a21b8b1
felip607 <63021437+felip607@users.noreply.github.com>
2025-03-11 19:01:13 -0500

    primer guardado
    primer guardado del desarrollo del parcial

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const route = express.Router();
+
+let archivoJson = undefined;
+
+const pathFile = path.resolve('./resources/starwar.json');
+fs.readFile(pathFile, { encoding: 'utf8' }, (err, data) => {
+  if (!err) {
+    archivoJson = JSON.parse(data); // Asegúrate de parsear el JSON
+  } else {
+    archivoJson = null;
+  }
+});
+
+route.get('/', (req, res) => {
+  const { homeworld, species } = req.query;
+  let data = archivoJson;
+
+  if (homeworld) {
+    data = data.filter(personaje => personaje.homeworld === homeworld);
+  }
+
+  if (species) {
+    data = data.filter(personaje => personaje.species === species);
+  }
+
+  res.render('home', { data }); // Pasa archivoJson a la vista home.ejs
+});
+
+export default route;
